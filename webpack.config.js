c91fa615a2fcc3ded6d44074fb1c9211ff3fc548
Raman Singh <kontakt@customisingh-software.de>
2017-10-01 17:23:08 +0200

    Created npm/webpack project.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: {
+        techradar: './src/Radar.js',
+        sample: './sample/app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+                test: /\.js$/
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJSPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'sample/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+    devtool: "#source-map",
+    watch: true
+};
