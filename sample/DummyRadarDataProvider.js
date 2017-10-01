2cdf258bdedfad73bb7c7191768964666b11e030
Raman Singh <kontakt@customisingh-software.de>
2017-10-01 17:23:29 +0200

    Implement sample.

---
+++ sample/DummyRadarDataProvider.js
@@ -0,0 +1,109 @@
+'use strict';
+
+export default class DummyRadarDataProvider {
+    constructor() {
+        this.data = [
+            {
+                "id" : 1,
+                "radius" : 1,
+                "angle" : 45,
+                "label" : "HBase",
+                "link" : "http://google.de"
+            },
+            {
+                "id" : 2,
+                "radius" : 0.6,
+                "angle" : 120,
+                "label" : "Apache Kafka",
+                "link" : ""
+            }
+        ];
+    }
+
+    load() {
+        return new Promise(function(resolve, reject) {
+            window.setTimeout(function() {
+                resolve(this.data);
+            }.bind(this), 100);
+        }.bind(this));
+    }
+
+    updateItem(item) {
+        return new Promise(function(resolve, reject) {
+            resolve(item);
+        })
+    }
+
+    addItem(radius, angle) {
+        return new Promise(function(resolve, reject) {
+            var label = prompt("Please enter the label of the new blip:", "");
+            if (label === null || label === "") {
+                reject();
+            } else {
+                this.data.push({"id" : this.data.length + 1, "radius" : radius, "angle" : angle, "label" : label});
+                resolve(this.data[this.data.length - 1]);
+            }
+        }.bind(this));
+    }
+
+    getMetaData() {
+        return {
+            "quadrants" : [
+                {
+                    "id" : 0,
+                    "name" : "Tools",
+                    "color" : {
+                        "blip" : "#86B782",
+                        "text" : "#000000"
+                    }
+                },
+                {
+                    "id" : 1,
+                    "name" : "Techniques",
+                    "color" : {
+                        "blip" : "#1EBCCD",
+                        "text" : "#000000"
+                    }
+                },
+                {
+                    "id" : 2,
+                    "name" : "Platforms",
+                    "color" : {
+                        "blip" : "#F38A3E",
+                        "text" : "#000000"
+                    }
+                },
+                {
+                    "id" : 3,
+                    "name" : "Languages & Frameworks",
+                    "color" : {
+                        "blip" : "#B32059",
+                        "text" : "#000000"
+                    }
+                },
+            ],
+            "states" : [
+                {
+                    "id" : 0,
+                    "name" : "ADOPT",
+                    "backgroundColor" : "#BFC0BF"
+                },
+                {
+                    "id" : 1,
+                    "name" : "TRIAL",
+                    "backgroundColor" : "#CBCCCB"
+                },
+                {
+                    "id" : 2,
+                    "name" : "ASSESS",
+                    "backgroundColor" : "#D7D8D6"
+                },
+                {
+                    "id" : 3,
+                    "name" : "HOLD",
+                    "backgroundColor" : "#E4E5E4"
+                },
+            ]
+        }
+    }
+}
---
+++ sample/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import * as d3 from 'd3';
+import Radar from './../src/Radar';
+import DummyRadarDataProvider from './DummyRadarDataProvider';
+
+var radarOptions = {
+	canvasWidth : 1300,
+	enableMove : true,
+	enableAddNew : true
+}
+
+var radarDataProvider = new DummyRadarDataProvider();
+var radar = new Radar("#techradarContainer", radarDataProvider, radarOptions);
+radar.render();
+
+document.getElementById("editButton").addEventListener("click", function() {
+	radar.setEditMode(!radar.getEditMode());
+});
